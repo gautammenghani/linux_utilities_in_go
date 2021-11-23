e5048cdd02bd014debc178aa262846fc87fd3c7f
gum3ng <gum3ng@protonmail.com>
2021-11-23 21:48:23 +0530

    Added support for -b in cat

--- cat.go
+++ cat.go
@@ -8,51 +8,76 @@ import (
     "path/filepath"
     "errors"
     "strings"
+    "strconv"
 )
 
-func ReadFile(fileNames []string) (string, error){
+func NumberNonBlankLines(contents string) (string) {
+  lines := strings.Split(contents, "\n")
+  cntr := 1
+  var modifiedContents string
+  for _, line := range lines {
+    if(len(line)>0) {
+      modifiedContents += (strconv.Itoa(cntr) + " " + line + "\n")
+      cntr += 1
+     } else {
+        modifiedContents += "\n"
+     }
+  }
+  return modifiedContents
+}
+
+func ReadFile(fileNames []string, switches []string) (string, error){
     var contents string
     for _, file := range fileNames {
-        //Get absolute file path
-        fileName, err := filepath.Abs(file)
-        if (err != nil) {
-            errMsg := fmt.Sprintf ("File not found: %s\n", err)
-            fmt.Println (errMsg)
-            os.Exit(0)
-        }
-        //Read file
-        //s := fmt.Sprintf("Reading file : %s\n", fileName)
-        //fmt.Println(s)
-        data, err := ioutil.ReadFile(fileName)
-        if (err != nil) {
-            return contents, errors.New(fmt.Sprintf("Could not read file : %s\n", err))
-        } else {
-            contents += "\n" + "[+] Reading file : "+fileName + "\n" + string(data)
-        }
+      //Get absolute file path
+      fileName, err := filepath.Abs(file)
+      if (err != nil) {
+          errMsg := fmt.Sprintf ("File not found: %s\n", err)
+          fmt.Println (errMsg)
+          os.Exit(0)
+      }
+      data, err := ioutil.ReadFile(fileName)
+      if (err != nil) {
+          return contents, errors.New(fmt.Sprintf("Could not read file : %s\n", err))
+      } else {
+          contents += "\n" + "[+] Reading file : "+fileName + "\n" + string(data)
+      }
+    }
+    //modify the contents as per the switches
+    for _, param := range switches {
+      switch param{
+        case "b": 
+          contents = NumberNonBlankLines(contents)
+        default: 
+        fmt.Println(param + " switch is not supported yet")
+      }
     }
     return contents, nil
 }
 
-func GetContents(userInput []string) (string, error) {
-  //Extract the switches from cmnd line args
+func ParseCommand(userInput []string) (string, error) {
   var switches[]string
+  var files[]string
   for _,elem := range userInput {
     if(string(elem[0])=="-") {
+      //get the switches
       t := strings.Split(elem[1:], "")
       for _,st := range t {
         switches = append(switches, st)
       }
+    } else {
+      //get the file names
+      files = append(files,elem)
     }
   }
-  fmt.Println(switches)
-  contents, err := ReadFile(userInput)
-      if (err != nil) {
-          fmt.Println (contents)
-          errMsg := fmt.Sprintf ("Error occurred when reading file: %s\n", err)
-          fmt.Println (errMsg)
-          os.Exit(0)
-      } 
-      return "hello", nil  
+  contents, err := ReadFile(files, switches)
+  if (err != nil) {
+      fmt.Println (contents)
+      errMsg := fmt.Sprintf ("Error occurred when reading file: %s\n", err)
+      fmt.Println (errMsg)
+      os.Exit(0)
+  } 
+  return contents, nil  
 }
 
 func main() {
@@ -64,7 +89,7 @@ func main() {
             fmt.Println(input)
         } 
     } else {
-      contents, err := GetContents(os.Args[1:])
+      contents, err := ParseCommand(os.Args[1:])
       if (err != nil) {
           fmt.Println (contents)
           errMsg := fmt.Sprintf ("Error occurred when reading file: %s\n", err)
